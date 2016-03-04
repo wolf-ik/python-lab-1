0f277d51271cdd9da473548a714f8f5a3c062b1d
Vlad Velichko <ya.super.vladik@gmail.com>
2016-03-04 13:10:30 +0300

    +wc

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import argparse, fibonacci, sort, storage
+import argparse, fibonacci, sort, storage, wordcount
 
 
 def fibonacci_handler(args):
@@ -15,8 +15,23 @@ def storage_handler(args):
     strg.run()
 
 
+def word_count_handler(args):
+    with open(args.filename, 'r') as f:
+        k, n = args.k, args.n
+        wc = wordcount.WordCount(str(f.read()))
+        for key, value in wc.get_words():
+            print('{}: {}'.format(key, value))
+        print('*************')
+        print('Average words in sentence: %f' % wc.get_average_words())
+        print('Median: %s - %d' % wc.get_median())
+        print('*************')
+        print('Top {} {}-gramm:'.format(k, n))
+        for key, value in wc.n_gramm(k, n):
+            print('{}: {}'.format(key, value))
+
+
 def parse_args():
-    parser = argparse.ArgumentParser(description='Parser description.')
+    parser = argparse.ArgumentParser(description='My wonderful description.')
     subparsers = parser.add_subparsers()
 
     parser_fibonacci = subparsers.add_parser('fibonacci', help='Generate fibonacci sequence.')
@@ -28,8 +43,14 @@ def parse_args():
     parser_sort.add_argument('array', type=str, help='Input array.')
     parser_sort.set_defaults(func=my_sort_handler)
 
-    parser_storage = subparsers.add_parser('storage', help='Interaktive storage.')
+    parser_storage = subparsers.add_parser('storage', help='Interactive storage.')
     parser_storage.set_defaults(func=storage_handler)
+
+    parser_word_count = subparsers.add_parser('wc', help='Word count.')
+    parser_word_count.add_argument('filename', type=str, help='Path to input file.')
+    parser_word_count.add_argument('k', type=int, help='Top K N-gramm.')
+    parser_word_count.add_argument('n', type=int, help='Length N-gramm.')
+    parser_word_count.set_defaults(func=word_count_handler)
 
     return parser.parse_args()
 
---
+++ src/wordcount.py
@@ -0,0 +1,35 @@
+import re, collections
+
+
+class WordCount(object):
+    _words = collections.Counter()
+    _average_words_in_sentence = 0
+    _word_excludes = ('', ' ', '-', '\n')
+
+    def __init__(self, text):
+        text = re.sub('[!?]', '.', text)
+        text = re.sub('\.\.\.', '.', text)
+        text = re.sub('[\n:,;()]', '', text)
+        sentences = text.split('.')
+        words = [word for sentence in sentences for word in sentence.split(' ') if word not in self._word_excludes]
+        self._average_words_in_sentence = float(len(words)) / len(sentences)
+        for word in words:
+            self._words[word] += 1
+
+    def get_words(self):
+        return self._words.most_common(len(self._words))
+
+    def get_average_words(self):
+        return self._average_words_in_sentence
+
+    def get_median(self):
+        pos = len(self._words) / 2
+        t = self._words.most_common(pos)[pos - 1]
+        return t[0], t[1]
+
+    def n_gramm(self, k=10, n=4):
+        counter = collections.Counter()
+        for word in self._words:
+            for i in xrange(len(word) - n):
+                counter[word[i:i+n]] += 1
+        return counter.most_common(k)
